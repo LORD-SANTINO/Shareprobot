b2e938e14b2451a31b2db242e61b58aac8b3ab0c
LORD-SANTINO <mikkyrose297@gmail.com>
2025-11-18 14:31:20 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+FORCE_JOIN = os.getenv("FORCE_JOIN_CHANNEL")
+CREATOR = os.getenv("CREATOR_USERNAME", "@daxkings")
+REFERRALS_NEEDED = 10
