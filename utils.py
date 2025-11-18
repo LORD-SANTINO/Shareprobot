3b7d73d9258300b7702d2befbc59e29226a16d7c
LORD-SANTINO <mikkyrose297@gmail.com>
2025-11-18 15:16:44 +0100

    Update share and referral link generation to use BOT_USERNAME

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
-from config import BOT, FORCE_JOIN
+from config import BOT_USERNAME, FORCE_JOIN
 
 def generate_share_link(delete_code):
-    return f"https://t.me/{BOT.lstrip('@')}?start=lock_{delete_code}"
+    return f"https://t.me/{BOT_USERNAME.lstrip('@')}?start=lock_{delete_code}"
 
 def generate_referral_link(user_id):
-    return f"https://t.me/{BOT.lstrip('@')}?start=ref_{user_id}"
+    return f"https://t.me/{BOT_USERNAME.lstrip('@')}?start=ref_{user_id}"
 
 def get_expiry_text(expiry):
     if not expiry:
